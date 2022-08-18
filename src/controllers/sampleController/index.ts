80bd9a3b6bfeba644ea46e6f4b242040d0a79f41
samueltuyizere <samueltuyizere0@gmail.com>
2022-08-18 19:14:39 +0200

    added sample controller
    Signed-off-by: samueltuyizere <samueltuyizere0@gmail.com>

---
+++ src/controllers/sampleController/index.ts
@@ -0,0 +1,8 @@
+/**
+ *
+ * @param param this will be parameters that the function will take in
+ * @returns
+ */
+export async function sampleFunctions (param: string): Promise<string> {
+  return param
+}
